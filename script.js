3be08ed71855278785f99ff745b0f97db9f28571
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-08-06 21:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+const form = document.getElementById('form');
+const baseUrl = 'https://localhost:8080/';
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+
+    let tempUrl = baseUrl + '?';
+
+    if(name && year){
+        tempUrl += `name=${name}&year=${year}`;
+    }
+    else if(name){
+        tempUrl += `name=${name}`;
+    }
+    else tempUrl += `year=${year}`;
+
+    document.getElementById('head').innerText = tempUrl;
+})
